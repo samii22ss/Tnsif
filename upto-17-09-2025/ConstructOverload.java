af382fe53c10ef878bd682b9becd96a86475000c
samii22ss <azlansami38@gmail.com>
2025-09-22 01:42:58 +0530

    Update ConstructOverload.java

--- upto-17-09-2025/ConstructOverload.java
+++ upto-17-09-2025/ConstructOverload.java
@@ -1,26 +1,26 @@
-package com.tns.septseventeen;
+package com.tns.conoverload;
 class practise{
-	practise(int a , int b){
+	water(int a , int b){
 		System.out.println("This is constructor with 2 parameters");
 		int product = a*b;
 		System.out.println(product);
 	}
-	practise(int a, int b, int c) {
+	water(int a, int b, int c) {
 		System.out.println("This is constructor with 3 parameters");
 		int product = a*b*c;
 		System.out.println(product);
 	}
-	practise(String a, String b) {
+	water(String a, String b) {
 		System.out.println("This is constructor with 2 String parameters");
 		String res = a+" "+b;
 		System.out.println(res);
 	}
-	practise(String a, int b) {
+	water(String a, int b) {
 		System.out.println("This is constructor with a String and int parameters");
 		String res = a+" "+b;
 		System.out.println(res);
 	}
-	practise(int a, String b) {
+	water(int a, String b) {
 		System.out.println("This is constructor with a int and String parameters");
 		String res = a+" "+b;
 		System.out.println(res);
@@ -29,11 +29,12 @@ class practise{
 public class ConstructOverload {
 
 	public static void main(String[] args) {
-		practise obj1 = new practise(1,2);
-		practise obj2 = new practise(1,2,3);
+		water obj1 = new water(1,2);
+		water obj2 = new water(1,2,3);
 		
 	
 
 	}
 
 }
+
