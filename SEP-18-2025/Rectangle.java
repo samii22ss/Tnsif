de3832bdd233f5145b12618db66b2967690c996e
samii22ss <azlansami38@gmail.com>
2025-09-22 01:23:30 +0530

    Update Rectangle.java

--- SEP-18-2025/Rectangle.java
+++ SEP-18-2025/Rectangle.java
@@ -1,7 +1,7 @@
-package com.tns.septeighteen;
+package com.tns.azlansami;
 
 public class Rectangle extends Shape {
-	int height = 2, width = 3;
+	int height = 5, width = 3;
 	public Rectangle(int h , int w) {
 		this.height=h;
 		this.width=w;
@@ -10,3 +10,4 @@ public class Rectangle extends Shape {
 		super.area = height*width;
 	}
 }
+
