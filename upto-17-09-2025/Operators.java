6dfbeb797fa024dc3aecc7d44fc9847b1c768a36
samii22ss <azlansami38@gmail.com>
2025-09-22 01:35:38 +0530

    Update Operators.java

--- upto-17-09-2025/Operators.java
+++ upto-17-09-2025/Operators.java
@@ -1,9 +1,10 @@
-package com.tnstasksjava;
+package com.operator;
 
 public class Operators {
 	public static void main(String[] args) {
-	int a =10;
+	int a =14;
 	int b = ++a;
 	 System.out.println(a+""+b);
 	}
 }
+
