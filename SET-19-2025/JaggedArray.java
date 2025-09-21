10c073d094f37fcf36f9e23346cd770e208d463e
samii22ss <azlansami38@gmail.com>
2025-09-22 01:27:43 +0530

    Update JaggedArray.java

--- SET-19-2025/JaggedArray.java
+++ SET-19-2025/JaggedArray.java
@@ -1,12 +1,12 @@
-package com.tns.septnineteen;
+package com.tns.azlansami;
 
 public class JaggedArray {
 
 	public static void main(String[] args) {
 		int[][] nums = new int[3][];
-		nums[0] = new int[3];
-		nums[1] = new int[4];
-		nums[2] = new int[1];
+		nums[0] = new int[1];
+		nums[1] = new int[2];
+		nums[2] = new int[3];
 
 		for(int i=0;i<nums.length;i++) {
 			for(int j=0;j<nums[i].length;j++) {
@@ -23,3 +23,4 @@ public class JaggedArray {
 	}
 
 }
+
