28423fd478ce9fb1d379e01f24b42020b47a045a
samii22ss <azlansami38@gmail.com>
2025-09-22 01:32:23 +0530

    Update int-to-int.java

--- SET-19-2025/int-to-int.java
+++ SET-19-2025/int-to-int.java
@@ -1,10 +1,11 @@
-package com.tns.septnineteen;
+package com.tns.azlansami;
 
 public class Int2Int implements Interface2 {
 	public void int1() {
-			System.out.println("Method from Interface 1");
+			System.out.println("Call Method from Interface 1");
 		}
 	public void int2() {
-		System.out.println("Method from Interface 2");
+		System.out.println("Call Method from Interface 2");
 	}
 }
+
